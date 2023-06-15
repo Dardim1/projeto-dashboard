a2a1e6df8925db8e9b2322a2e52aa0ee94f9f946
Dardim1 <136619471+Dardim1@users.noreply.github.com>
2023-06-15 13:45:26 -0300

    novo site

---
+++ orders.js
@@ -0,0 +1,46 @@
+const Orders = [
+    {
+        producName: 'Foldable Mini Drone',
+        productNumber: '85631' ,
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        producName: 'LARVANDER KF102 Drone',
+        productNumber: '36378' ,
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        producName: ' Ruko F11 Pro Drone ',
+        productNumber: '49347' ,
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+
+    {
+        producName: 'Drone with Camera Drone',
+        productNumber: '96996' ,
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        producName: 'GPS 4K Drone',
+        productNumber: '22821' ,
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        producName: 'DJI Air 2S',
+        productNumber: '81475' ,
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },    {
+        producName: 'Lazenge Drone',
+        productNumber: '00482' ,
+        paymentStatus: 'Paid',
+        shipping: 'Delevired'
+    }
+
+
+]
